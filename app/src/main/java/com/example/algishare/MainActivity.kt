6a89d18b5b698279b94d239ac7359e06b3d3902b
Ömer İmamoğlu <imamogluomer@outlook.com>
2024-08-03 23:34:35 +0300

    kotlin verison of my algishare app

--- app/src/main/java/com/example/algishare/MainActivity.kt
+++ app/src/main/java/com/example/algishare/MainActivity.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AlgiShareTheme {
-                Scaffold(modifier = Modifier.padding(all = Dp(50f))) {
+                Scaffold(modifier = Modifier.padding(all = Dp(0f))) {
                     HomeScreen(
                         name = ""
                     )
@@ -34,9 +38,18 @@ class MainActivity : ComponentActivity() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(name: String){
-    Scaffold (){}
+
+
+    TopAppBar(modifier = Modifier.padding(),title = { Row {
+        Text(text = "Good Evening!")
+        IconButton(onClick = {}) {
+            Icon..
+        }
+    }
+    })
 }
 
 @Composable
